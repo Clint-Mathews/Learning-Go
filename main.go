f39e36e8c8d195cd14d842d6d3292d1a161f62b6
Clint-Mathews <mathewsclint28@gmail.com>
2022-01-10 22:13:52 +0530

    Updated with loops, slice, array, conditional if/else and switch

--- main.go
+++ main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	conferenceName := "Go Conference"
@@ -13,10 +16,15 @@ func main() {
 	// fmt.Printf("%v \n",remaingTickets)
 	// fmt.Println(&remaingTickets)
 
+	for{
 	var firstName string
 	var lastName string
 	var email string
 	var userTickets uint
+	// var bookings []string
+	// var bookings =  []string{}
+	bookings := []string{}
+
 	// Ask for user input
 	fmt.Println("Enter your first name:")
 	fmt.Scan(&firstName);
@@ -27,10 +35,64 @@ func main() {
 	fmt.Println("Enter your no of tickets:")
 	fmt.Scan(&userTickets);
 
+	isValidName :=len(firstName) >= 2 && len(lastName) >=2
+	isValidEmail := strings.Contains(email, "@")
+	isValidTicketCount := userTickets > 0 && userTickets <= remaingTickets
+
+	if isValidName && isValidEmail && isValidTicketCount  {
+
+	// bookings[0] = firstName + " " + lastName
+	bookings = append(bookings, firstName + " " + lastName)
 	remaingTickets = remaingTickets - userTickets
+
+	// fmt.Printf("Whole Slice %v \n", bookings)
+	// fmt.Printf("Bookings 1st value %v \n", bookings[0])
+	// fmt.Printf("Bookings type %T \n", bookings)
+	// fmt.Printf("Bookings size %v \n", len(bookings))
 
 	fmt.Printf("%v %v with %v booked %v tickets \n",firstName,lastName,email ,userTickets)
 	fmt.Println("Thank you! You will recive the confirmation email.")
 	fmt.Printf("Total Tickets %v and Avaliable tickets %v \n", conferenceTickets, remaingTickets)
 
+	firstNames := []string{}
+
+	for _,name := range bookings {
+		var firstName = strings.Fields(name)
+		firstNames = append(firstNames,firstName[0]) 
+	}
+
+	fmt.Printf("All FirstName of Bookings : %v \n",firstNames)
+	if remaingTickets <= 0 {
+		// Breaks the loop
+		fmt.Println("Conference booked out!")
+		break
+	}
+// } else if userTickets == remaingTickets {
+
+// 	// Do something
+// } 
+} else{
+	if !isValidName {
+		fmt.Println("First Name or Last Name is too short")
+	}
+	if !isValidEmail {
+		fmt.Println("Email format is wrong")
+	}
+	if !isValidTicketCount {
+		fmt.Println("Requested ticket number is wrong")
+	}
+			// fmt.Println("Invalid Input Data")
+		// continue
+}
+// city := "London"
+// switch city {
+// 	case "London":
+// 		//Execute on London city 
+// 	case "Kochi","Aluva":
+// 		//Execute on Kochi and Aluva
+// 	default:
+// 		// Default block
+// 		fmt.Println("Invalid city")
+// }
+}
 }
